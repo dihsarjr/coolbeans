4837d9ea7544c6d4fba1b77530a11fcfb6c8cbc5
dihsarjr <mohammed1rashid786@gmail.com>
2023-11-05 14:32:39 +0530

    Initial Commit

---
+++ CoolBeans/CoolBeansApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolBeansApp.swift
+//  CoolBeans
+//
+//  Created by Mohammed Rashid on 05/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CoolBeansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
